0b270d2bda35f79ba553f79901412e95ba8215de
Vaibhav <vaibhavchaubey786@gmail.com>
2025-05-26 02:03:58 +0530

    Add Clerk authentication integration and update dependencies

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 import NavItems from './NavItems';
 
 const Navbar = () => {
@@ -13,7 +13,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
